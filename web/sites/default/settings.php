a87d5d9b556fefd854240583f14845823b4c613d
Tyler Fahey <twfahey1@gmail.com>
2020-02-27 13:53:08 -0600

    Comment out redis

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -41,20 +41,20 @@ $settings['hash_salt'] = 'T3hQMWuGXx3FP60mgjCffNEKi9hhXLToRH0XgFQ-sypAoxbLLu3zZo
 
 // Configure Redis
 
-if (defined('PANTHEON_ENVIRONMENT')) {
-  // Include the Redis services.yml file. Adjust the path if you installed to a contrib or other subdirectory.
-  $settings['container_yamls'][] = 'modules/redis/example.services.yml';
+// if (defined('PANTHEON_ENVIRONMENT')) {
+//   // Include the Redis services.yml file. Adjust the path if you installed to a contrib or other subdirectory.
+//   $settings['container_yamls'][] = 'modules/redis/example.services.yml';
 
-  //phpredis is built into the Pantheon application container.
-  $settings['redis.connection']['interface'] = 'PhpRedis';
-  // These are dynamic variables handled by Pantheon.
-  $settings['redis.connection']['host']      = $_ENV['CACHE_HOST'];
-  $settings['redis.connection']['port']      = $_ENV['CACHE_PORT'];
-  $settings['redis.connection']['password']  = $_ENV['CACHE_PASSWORD'];
+//   //phpredis is built into the Pantheon application container.
+//   $settings['redis.connection']['interface'] = 'PhpRedis';
+//   // These are dynamic variables handled by Pantheon.
+//   $settings['redis.connection']['host']      = $_ENV['CACHE_HOST'];
+//   $settings['redis.connection']['port']      = $_ENV['CACHE_PORT'];
+//   $settings['redis.connection']['password']  = $_ENV['CACHE_PASSWORD'];
 
-  $settings['cache']['default'] = 'cache.backend.redis'; // Use Redis as the default cache.
-  $settings['cache_prefix']['default'] = 'pantheon-redis';
+//   $settings['cache']['default'] = 'cache.backend.redis'; // Use Redis as the default cache.
+//   $settings['cache_prefix']['default'] = 'pantheon-redis';
 
-  // Set Redis to not get the cache_form (no performance difference).
-  $settings['cache']['bins']['form']      = 'cache.backend.database';
-}
+//   // Set Redis to not get the cache_form (no performance difference).
+//   $settings['cache']['bins']['form']      = 'cache.backend.database';
+// }
